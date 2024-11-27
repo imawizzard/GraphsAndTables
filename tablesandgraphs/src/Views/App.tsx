0cd6e6120ae1b1bb8177b41362365870a0767588
ima_wizzard <aitor.vannevel@gmail.com>
2024-11-27 11:51:52 +0100

    added graph

--- tablesandgraphs/src/Views/App.tsx
+++ tablesandgraphs/src/Views/App.tsx
@@ -1,10 +1,12 @@
 import "../Layout/App.css";
 import Table from "../Components/Table";
+import Graph from "../Components/Graph";
 
 function App() {
   return (
     <div className="App">
       <Table />
+      <Graph />
     </div>
   );
 }
