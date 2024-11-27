2a774b8e228211c0dd9a81f4ecd69e57239b0b0b
ima_wizzard <aitor.vannevel@gmail.com>
2024-11-27 09:58:14 +0100

    added table

--- tablesandgraphs/src/Components/Table.tsx
+++ tablesandgraphs/src/Components/Table.tsx
@@ -1,57 +1,52 @@
 import DataTable from "react-data-table-component";
+import "../Layout/Table.css"; // Voor extra algemene stijlen
+import { TableStyles } from "react-data-table-component";
 
 interface Row {
   id: number;
-  fullName: string;
-  height: string;
-  weight: string;
+  Jaar: number;
+  Winst: number;
+  Verlies: number;
 }
 
 function Table() {
   const rows: Row[] = [
-    {
-      id: 1,
-      fullName: "John Doe",
-      height: "1.75m",
-      weight: "89kg",
-    },
-    {
-      id: 2,
-      fullName: "Jane Doe",
-      height: "1.64m",
-      weight: "55kg",
-    },
-    {
-      id: 3,
-      fullName: "Sheera Maine",
-      height: "1.69m",
-      weight: "74kg",
-    },
+    { id: 1, Jaar: 2015, Winst: 4000, Verlies: 1500 },
+    { id: 2, Jaar: 2016, Winst: 5000, Verlies: 2000 },
+    { id: 3, Jaar: 2017, Winst: 7000, Verlies: 3000 },
+    { id: 4, Jaar: 2018, Winst: 8000, Verlies: 4000 },
+    { id: 5, Jaar: 2019, Winst: 6000, Verlies: 2500 },
+    { id: 6, Jaar: 2020, Winst: 9000, Verlies: 3500 },
+    { id: 7, Jaar: 2021, Winst: 10000, Verlies: 4500 },
+    { id: 8, Jaar: 2022, Winst: 11000, Verlies: 5000 },
+    { id: 9, Jaar: 2023, Winst: 12000, Verlies: 5500 },
+    { id: 10, Jaar: 2024, Winst: 13000, Verlies: 6000 },
   ];
 
   const columns = [
     {
-      name: "ID",
-      selector: (row: Row) => row.id,
+      name: "Jaar",
+      selector: (row: Row) => row.Jaar,
     },
     {
-      name: "Full Name",
-      selector: (row: Row) => row.fullName,
+      name: "Winst",
+      selector: (row: Row) => row.Winst,
     },
     {
-      name: "Height",
-      selector: (row: Row) => row.height,
-    },
-    {
-      name: "Weight",
-      selector: (row: Row) => row.weight,
+      name: "Verlies",
+      selector: (row: Row) => row.Verlies,
     },
   ];
 
   return (
     <>
       <div className="container my-5">
-        <DataTable columns={columns} data={rows} />
+        <DataTable
+          columns={columns}
+          data={rows}
+          fixedHeader
+          title="Inkomsten / uitgaven"
+        />
       </div>
     </>
   );
